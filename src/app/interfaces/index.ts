a08dc32bdcfa7c9058b92cfcb95e8902437c025d
Raymond Gigliotti <ray@firestitch.com>
2025-07-23 06:57:48 -0400

    modify interface exports to include crm-config and reorder crm-lead

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,8 +1,9 @@
 export * from './account';
 export * from './add-actvity-menu-item';
 export * from './crm-channel';
-export * from './crm-lead';
+export * from './crm-config';
 export * from './crm-lead-config';
+export * from './crm-lead';
 export * from './crm-leads-config';
 export * from './crm-log';
 export * from './crm-note';
@@ -11,4 +12,3 @@ export * from './lead-activity-type-preview';
 export * from './lead-secondary-container';
 export * from './lead-tab';
 export * from './leads-column';
-
