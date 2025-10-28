91e996eafb793e22af0856772ce45016e50c3783
Deployment Manager <sysadmin@firestitch.com>
2025-10-28 10:05:05 +0000

    Migrate to standalone

--- playground/app/components/examples/examples.component.ts
+++ playground/app/components/examples/examples.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FsExampleModule } from '@firestitch/example';
+import { LeadsComponent } from '../leads/leads.component';
 
 
 @Component({
-  templateUrl: './examples.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './examples.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [FsExampleModule, LeadsComponent],
 })
 export class ExamplesComponent {
 }
